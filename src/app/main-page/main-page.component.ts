aadd14c8f4c1a633df72c1e3031ba4b359a62d26
Juan Camilo Salazar Serna <jcss1462@gmail.com>
2019-06-01 17:37:09 -0500

    1 localstorage implementado

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -14,6 +14,8 @@ export class MainPageComponent implements OnInit {
 
   artists: Artist[];
 
+  errMsg:string;
+
   constructor(private _Artistas:ArtistasService) {}
 
   ngOnInit() {
@@ -24,7 +26,23 @@ export class MainPageComponent implements OnInit {
         data[idx].portada=this.bufferToBase64(data[idx]["imagen_principal"]["imgBase64"]);
       }
       this.artists=data;
-    });
+      
+      console.log("extraxion de datos exitosa");
+      localStorage.setItem("artistas",JSON.stringify(data));
+    },(error) =>{ 
+      this.errMsg = error.message
+      console.log(this.errMsg);
+      console.log("error de conexion\nintento de usar el local storage");
+
+      if(localStorage.getItem('artistas')==null){
+        console.log('Local storage vacio')
+      }else{
+        this.artists=JSON.parse(localStorage.getItem('artistas'));
+        console.log('Local storage usado')
+      }
+
+    }
+    );
   }
 
   
